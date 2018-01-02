14e407affee27a4039fbb52c613179091efc875b
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-02 14:38:14 +0200

    pohja valmiina

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+
+    return (
+        <div>
+            <h2>Anna palautetta</h2>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
